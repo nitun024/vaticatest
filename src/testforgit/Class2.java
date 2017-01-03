c84606b72c85df2e14af0faeff1c4d8b4747c52c
KusumMishra <kusum.mishra@quovantis.com>
2017-01-03 17:04:45 +0530

    Second conflict

--- src/testforgit/Class2.java
+++ src/testforgit/Class2.java
@@ -4,6 +4,6 @@ public class Class2 {
 
 	public void method2() {
 		
-		System.out.print("Hahahaha i dont like the usual way.!.");
+		System.out.print("Hahahaha i dont like the usual way.!.fgfg");
 	} 
 }
