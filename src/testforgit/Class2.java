3e879323680fc9f78d0490cb11b3b96e56ee8b6c
nitun024 <nitun024@gmail.com>
2017-01-03 16:57:38 +0530

    New push 1

--- src/testforgit/Class2.java
+++ src/testforgit/Class2.java
@@ -3,7 +3,7 @@ package testforgit;
 public class Class2 {
 
 	public void method2() {
-		System.out.println("HAHAvsdgsgsA");
-		System.out.println("Hahahaha i dont like the usual way.!.");
+		
+		System.out.print("Hahahaha i dont like the usual way.!.");
 	} 
 }
